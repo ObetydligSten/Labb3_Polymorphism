38fd0226920b0b52528285083d525cef070ad616
Rasmus Glasell Johansson <ragl025@gmail.com>
2023-11-01 00:03:41 +0100

    Add project files.

---
+++ Labb3_Polymorphism/Geometry.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb3_Polymorphism
+{
+    public class Geometry
+    {
+        public virtual double Area()
+        {            
+            return 0;
+        }
+    }
+    public class Square : Geometry
+    {
+        public double _Side;
+        public Square()
+        {
+            _Side = 7.34;
+        }
+        public override double Area()
+        {
+            return _Side * _Side;
+        }
+    }
+    public class Circle : Geometry
+    {
+        public double _Radius;
+        public Circle()
+        {
+            _Radius = 4;
+        }
+        public override double Area()
+        {
+            return Math.PI * _Radius * _Radius;
+        }
+    }
+    public class Rectangle : Geometry
+    {
+        public double _Length;
+        public double _Width;
+
+        public Rectangle()
+        {
+            _Length = 9.65;
+            _Width = 5.36;
+        }
+        public override double Area()
+        {
+            return _Length * _Width;
+        }
+    }
+}
---
+++ Labb3_Polymorphism/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Labb3_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Geometry areaS = new Square();
+            Geometry areaC = new Circle();
+            Geometry areaR = new Rectangle();
+
+            Console.WriteLine($"Area Square : {areaS.Area()}\nArea Circle : {areaC.Area()}\nArea Rectangle : {areaR.Area()}");
+        }
+    }
+}
